0af015e308c727fe1cbbec324e1b5958deebef3e
Philippe35507 <phdefoy@gmail.com>
2023-07-02 21:57:15 +0100

    Controlled Elements: useState-onChange

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: true },
@@ -20,8 +22,10 @@ function Logo() {
 }
 
 function Form() {
+  const [description, setDescription] = useState("");
   function handleSubmit(e) {
     e.preventDefault();
+
     return;
   }
 
@@ -35,7 +39,12 @@ function Form() {
           </option>
         ))}
       </select>
-      <input type="text" placehotlder="Items..." />
+      <input
+        type="text"
+        placeholder="Items..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
       <button>Add</button>
     </form>
   );
